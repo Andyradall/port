3cb7c9b092da442d376330d694c263c331d253b0
[Andyradall] <[Andershr2@gmail.com]>
2019-02-10 21:03:15 +0100

    First commit Message

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const projects = document.querySelector("#projects");
+
+const db = firebase.database();
+const prosjekter = db.ref("testprosjekt");
+
+
+function visProsjekt(snap) {
+    const prosjekt = snap.val();
+
+    projects.innerHTML += `
+        <article class="entry">
+            <img class="entryImg" src="${prosjekt.bilder[0].url}" alt="">
+            <h2>${prosjekt.tittel}</h2>
+            <p> ${prosjekt.beskrivelse}</p>
+        </article>
+    `;
+
+}
+
+prosjekter.on("child_added", visProsjekt);
